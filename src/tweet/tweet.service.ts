411211cdb06b5bf6e327bcd91709f0f53264ee5f
kumar-github1 <kumarsssdsk@gmail.com>
2025-07-21 22:22:21 +0530

    Built in exceptions added to some extend

--- src/tweet/tweet.service.ts
+++ src/tweet/tweet.service.ts
@@ -1,5 +1,5 @@
 import { UsersService } from './../users/users.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTweetDto } from './dto/create-tweet.dto';
 import { UpdateTweetDto } from './dto/update-tweet.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -8,70 +8,75 @@ import { Repository } from 'typeorm';
 import { Hashtag } from 'src/hashtag/entities/hashtag.entity';
 import { HashtagService } from 'src/hashtag/hashtag.service';
 
-
 @Injectable()
 export class TweetService {
-  constructor(private readonly userService : UsersService,
-    private readonly hashtagService : HashtagService,
+  constructor(
+    private readonly userService: UsersService,
+    private readonly hashtagService: HashtagService,
     @InjectRepository(Tweet)
-    private tweetRepository : Repository<Tweet>,
-    
-  ) {
-
-  }
+    private tweetRepository: Repository<Tweet>,
+  ) {}
 
   async createTweet(createTweetDto: CreateTweetDto) {
     let user = await this.userService.getUserById(createTweetDto.userId);
-    if(!user) throw new Error('user not found');
-    let hashtags : Hashtag[]  = await this.hashtagService.findHashtags(createTweetDto.hashtags)
-    let tweet = this.tweetRepository.create({...createTweetDto,user,hashtags});
+    if (!user) throw new Error('user not found');
+    let hashtags: Hashtag[] = await this.hashtagService.findHashtags(
+      createTweetDto.hashtags,
+    );
+    let tweet = this.tweetRepository.create({
+      ...createTweetDto,
+      user,
+      hashtags,
+    });
     return await this.tweetRepository.save(tweet);
   }
 
   findAll() {
     return this.tweetRepository.find({
-      relations:{
-        user : true
-      }
+      relations: {
+        user: true,
+      },
     });
   }
 
-  findOne(id : number) {
+  async findOne(id: number) {
+    let user = await this.userService.getUserById(id);
+    if (!user)
+      throw new NotFoundException(`The user with userId ${id} is not found`);
     return this.tweetRepository.find({
-      where:{
-        user  : {
-          id 
-        }
+      where: {
+        user: {
+          id,
+        },
       },
       relations: {
-        user: true
-      }
-    })
+        user: true,
+      },
+    });
   }
 
   async update(updateTweetDto: UpdateTweetDto) {
-    let hastags = await this.hashtagService.findHashtags(updateTweetDto.hashtags??[]);
-    let tweet = await this.tweetRepository.findOneBy({id : updateTweetDto.id})
-    if(!tweet){
-      throw new Error("the user is not found")
+    let hastags = await this.hashtagService.findHashtags(
+      updateTweetDto.hashtags ?? [],
+    );
+    let tweet = await this.tweetRepository.findOneBy({ id: updateTweetDto.id });
+    if (!tweet) {
+      throw new Error('the user is not found');
     }
     tweet.text = updateTweetDto.text ?? tweet?.text;
     tweet.image = updateTweetDto.image ?? tweet.image;
     tweet.hashtags = hastags ?? [];
-    
+
     return await this.tweetRepository.save(tweet);
-    
   }
 
   async delete(id: number) {
-    let tweet = await this.tweetRepository.findOneBy({id});
-    if(!tweet) throw new Error("tweet not found");
+    let tweet = await this.tweetRepository.findOneBy({ id });
+    if (!tweet) throw new Error('tweet not found');
     await this.tweetRepository.delete(id);
     return {
       deleted: true,
-      status: "OK"
+      status: 'OK',
     };
   }
-
- 
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { forwardRef, Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  forwardRef,
+  Inject,
+  Injectable,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/createUser.dto';
 import { AuthService } from 'src/auth/auth.service';
 import { User } from './entities/user.entitiy';
@@ -8,70 +13,72 @@ import { Profile } from 'src/profile/entities/profile.entity';
 import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    @InjectRepository(Profile)
+    private profileRepository: Repository<Profile>,
+    private readonly configService: ConfigService,
+  ) {}
+  // public users: User[] = [{
+  //     id: 1,
+  //     name: 'John',
+  //     email: 'john@gmail.com',
+  //     password: '123456'
+  // },
+  // {
+  //     id: 2,
+  //     name: 'Jane',
+  //     email: 'jane@gmail.com',
+  //     password: '123456'
+  // }];
 
-    constructor(
-        @InjectRepository(User)
-        private userRepository: Repository<User>,
-        @InjectRepository(Profile)
-        private profileRepository: Repository<Profile>,
-        private readonly configService : ConfigService
-    ) {
+  async getUsers() {
+    const environment = this.configService.get('ENV_MODE');
+    console.log(environment);
+    return await this.userRepository.find({
+      relations: {
+        // profile : true
+      },
+    });
+  }
 
-    }
-    // public users: User[] = [{
-    //     id: 1,
-    //     name: 'John',
-    //     email: 'john@gmail.com',
-    //     password: '123456'
-    // },
-    // {
-    //     id: 2,
-    //     name: 'Jane',
-    //     email: 'jane@gmail.com',
-    //     password: '123456'
-    // }];
+  async getUserById(id: number) {
+    return await this.userRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+  }
+  public async createUser(userDto: CreateUserDto) {
+    //create profile
+    // userDto.profile = userDto.profile ?? {}
+    // let newProfile = this.profileRepository.create(userDto.profile);
+    //save profile
+    // newProfile = await this.profileRepository.save(newProfile);
 
-    async getUsers() {
-        const environment = this.configService.get("ENV_MODE");
-        console.log(environment)
-        return await this.userRepository.find({
-            relations:{
-                // profile : true
-            }
-        })
-    }
+    // //create user object
+    let user = await this.userRepository.find({
+      where: {
+        email: userDto.email,
+      },
+    });
 
-    async getUserById(id: number) {
-        return await this.userRepository.findOne({
-            where: {
-                id: id,
-            }
-        })
-    }
-    public async createUser(userDto: CreateUserDto) {
-        //create profile 
-        // userDto.profile = userDto.profile ?? {} 
-        // let newProfile = this.profileRepository.create(userDto.profile);
-        //save profile
-        // newProfile = await this.profileRepository.save(newProfile);
+    if (user)
+      throw new ConflictException('The user with this email already exists');
+    let newUser = this.userRepository.create(userDto);
 
-        // //create user object
-        let newUser = this.userRepository.create(userDto);
-
-        // //set profile
-        // newUser.profile = newProfile;
-        //save user profile
-        newUser = await this.userRepository.save(newUser);
-        return newUser;
-
-    }
-    async removeUser(id : number){
-        // let user = await this.userRepository.findOneBy({id})
-        await this.userRepository.delete(id);
-        // if(user)
-        // await this.profileRepository.delete(user.profile.id);
-        return {deleted : true}
-    }
+    // //set profile
+    // newUser.profile = newProfile;
+    //save user profile
+    newUser = await this.userRepository.save(newUser);
+    return newUser;
+  }
+  async removeUser(id: number) {
+    // let user = await this.userRepository.findOneBy({id})
+    await this.userRepository.delete(id);
+    // if(user)
+    // await this.profileRepository.delete(user.profile.id);
+    return { deleted: true };
+  }
 }
-
-
